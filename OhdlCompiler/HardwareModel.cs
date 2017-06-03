07790dbd13abfccf21df7ca0e3d2ab6bc3afa9f2
Hamish Milne <hamishmilne83@gmail.com>
2017-06-03 21:18:34 +0100

    Hardware model

---
+++ OhdlCompiler/HardwareModel.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using static OhdlCompiler.HardwareBinaryOp;
+
+namespace OhdlCompiler
+{
+    class HardwareModel
+    {
+	    public List<HardwareModule> Modules = new List<HardwareModule>();
+    }
+
+	abstract class HardwareExpression
+	{
+		public abstract void Emit(StringBuilder sb, ref int indent);
+
+		public virtual bool IsStatement => false;
+	}
+
+	class HardwareNamedValue : HardwareExpression
+	{
+		public ImmutableArray<string> Name { get; set; }
+
+		public override void Emit(StringBuilder sb, ref int indent)
+		{
+			foreach (var n in Name)
+				sb.Append(n).Append('$');
+		}
+	}
+
+    abstract class HardwareMember : HardwareNamedValue
+	{
+		public bool IsPublic { get; set; }
+
+		public virtual void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent);
+		}
+		public virtual void EmitAsOutput(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent);
+		}
+	}
+
+    class HardwareParameter : HardwareNamedValue
+	{
+        public bool IsOut { get; set; }
+        public int Size { get; set; }
+
+		public virtual void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent).Append(IsOut ? "output" : "input").Append(' ');
+			if (Size != 1)
+				sb.Append('[').Append(Size).Append(":0] ");
+            Emit(sb, ref indent);
+			sb.Append(";\n");
+		}
+	}
+
+	class HardwareModuleInstance : HardwareMember
+	{
+		public HardwareModule Module { get; set; }
+        public List<KeyValuePair<string, int>> Parameters { get; } = new List<KeyValuePair<string, int>>();
+        public List<HardwareExpression> Arguments { get; } = new List<HardwareExpression>();
+
+		public override void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent);
+            Emit(sb, ref indent);
+			if (Parameters.Count > 0)
+			{
+				sb.Append(" #( ");
+				for (var i = 0; i < Parameters.Count; i++)
+				{
+					if (i > 0)
+						sb.Append(", ");
+					var pair = Parameters[i];
+					sb.Append('.').Append(pair.Key).Append('(').Append(pair.Value).Append(')');
+				}
+				sb.Append(")");
+            }
+			sb.Append(' ');
+            Emit(sb, ref indent);
+
+			sb.Append('(');
+			for (int i = 0; i < Arguments.Count; i++)
+			{
+				if (i > 0)
+					sb.Append(", ");
+                Arguments[i].Emit(sb, ref indent);
+			}
+            sb.Append(");\n");
+		}
+	}
+
+	class HardwareModuleParam : HardwareNamedValue
+	{
+		public int DefaultValue { get; set; }
+
+		public virtual void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent).Append("parameter ");
+            Emit(sb, ref indent);
+			sb.Append(" = ").Append(DefaultValue).Append(";\n");
+		}
+	}
+
+	class HardwareMethod : HardwareMember
+	{
+        public List<HardwareParameter> Parameters { get; } = new List<HardwareParameter>();
+        public HardwareBlock Block { get; set; }
+
+		public override void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent).Append("task ");
+            Emit(sb, ref indent);
+			sb.Append(";\n");
+			indent++;
+            foreach (var p in Parameters)
+                p.EmitDeclaration(sb, ref indent);
+			indent--;
+            Block.Emit(sb, ref indent);
+		}
+	}
+
+	class HardwareEvent
+	{
+        public bool Invert { get; set; }
+        public HardwareExpression Trigger { get; set; }
+        public HardwareStatement Statement { get; set; }
+
+		public virtual void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent).Append("always @(").Append(Invert ? "negedge" : "posedge").Append(' ');
+            Trigger.Emit(sb, ref indent);
+			sb.Append(")\n");
+            Statement.Emit(sb, ref indent);
+		}
+	}
+
+	class HardwareInput : HardwareMember
+	{
+        public int Size { get; set; }
+
+		public override void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+			base.EmitDeclaration(sb, ref indent);
+			sb.Append("input ");
+			if (Size != 1)
+				sb.Append('[').Append(Size).Append(":0] ");
+			sb.Append(";\n");
+		}
+	}
+
+	class HardwareRegister : HardwareMember
+	{
+        public int Size { get; set; }
+
+		public override void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+            base.EmitDeclaration(sb, ref indent);
+			sb.Append("reg ");
+            if(Size != 1)
+			    sb.Append("[").Append(Size).Append(":0] ");
+            Emit(sb, ref indent);
+			sb.Append(';');
+		}
+	}
+
+	
+
+	class HardwareWire : HardwareMember
+	{
+		public int Size { get; set; }
+        public HardwareExpression Expression { get; set; }
+
+		public override void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+			base.EmitDeclaration(sb, ref indent);
+            sb.Append("wire [").Append(Size).Append(":0] ");
+            Emit(sb, ref indent);
+			sb.Append(";\n").Append("assign ");
+            Emit(sb, ref indent);
+		}
+	}
+
+
+
+	enum HardwareUnaryOp
+	{
+		Plus,
+        Minus,
+        BitwiseNot,
+        BoolNot,
+
+	}
+
+	class HardwareUnary : HardwareExpression
+	{
+		private static readonly Dictionary<HardwareUnaryOp, char> Map = new Dictionary<HardwareUnaryOp, char>
+		{
+            {HardwareUnaryOp.Plus, '+' },
+            {HardwareUnaryOp.Minus, '-' },
+            {HardwareUnaryOp.BitwiseNot, '~' },
+            {HardwareUnaryOp.BoolNot, '!' },
+		};
+
+		public HardwareUnaryOp Op { get; set; }
+        public HardwareExpression RValue { get; set; }
+
+		public override void Emit(StringBuilder sb, ref int indent)
+		{
+			sb.Append(Map[Op]);
+            RValue.Emit(sb, ref indent);
+		}
+	}
+
+	enum HardwareBinaryOp
+	{
+		Add,
+		Sub,
+		Mul,
+		Div,
+		BitwiseOr,
+		BitwiseAnd,
+		BitwiseXor,
+		BoolOr,
+		BoolAnd,
+		BoolXor,
+		Assign,
+		AssignAdd,
+		AssignSub,
+		AssignMul,
+		AssignDiv,
+		AssignOr,
+		AssignAnd,
+		AssignXor,
+	}
+
+    class HardwareBinary : HardwareExpression
+	{
+        
+		private static readonly Dictionary<HardwareBinaryOp, string> Map = new Dictionary<HardwareBinaryOp, string>
+		{
+            { Add, "+" },
+            { Sub, "-" },
+            { Mul, "*" },
+            { Div, "/" },
+            { BitwiseOr, "|" },
+            { BitwiseAnd, "&" },
+            { BitwiseXor, "^" },
+            { BoolOr, "||" },
+            { BoolAnd, "&&" },
+            { BoolXor, "^" },
+            { Assign, "=" },
+            { AssignAdd, "+=" },
+            { AssignSub, "-=" },
+            { AssignMul, "*=" },
+            { AssignDiv, "/=" },
+            { AssignOr, "|=" },
+            { AssignAnd, "&=" },
+            { AssignXor, "^=" },
+		};
+
+		public HardwareBinaryOp Op { get; set; }
+        public HardwareExpression RValue { get; set; }
+        public HardwareExpression LValue { get; set; }
+		public override bool IsStatement => Op >= Assign;
+
+		public override void Emit(StringBuilder sb, ref int indent)
+		{
+			sb.Append('(');
+            LValue.Emit(sb, ref indent);
+			sb.Append(Map[Op]);
+            RValue.Emit(sb, ref indent);
+			sb.Append(')');
+		}
+	}
+
+	class HardwareCall : HardwareExpression
+	{
+		public HardwareMethod Method { get; set; }
+        public List<HardwareExpression> Arguments { get; } = new List<HardwareExpression>();
+		public override bool IsStatement => true;
+
+		public override void Emit(StringBuilder sb, ref int indent)
+		{
+			Method.Emit(sb, ref indent);
+			sb.Append('(');
+			for (int i = 0; i < Arguments.Count - 1; i++)
+			{
+				Arguments[i].Emit(sb, ref indent);
+				sb.Append(", ");
+			}
+			sb.Append(')');
+		}
+	}
+
+	abstract class HardwareStatement
+	{
+		public virtual void Emit(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent);
+		}
+	}
+
+	class HardwareStatementSingle
+	{
+        public HardwareExpression Expression { get; set; }
+
+		public virtual void Emit(StringBuilder sb, ref int indent)
+		{
+            if(!Expression.IsStatement)
+                throw new Exception($"Expression {Expression} is not a valid statement");
+			sb.Append('\t', indent);
+            Expression.Emit(sb, ref indent);
+			sb.Append(";\n");
+		}
+	}
+
+	class HardwareBlock : HardwareStatement
+	{
+		public List<HardwareStatement> Statements { get; } = new List<HardwareStatement>();
+
+		public override void Emit(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent).Append("begin\n");
+			indent++;
+			foreach(var m in Statements)
+                m.Emit(sb, ref indent);
+			indent--;
+			sb.Append('\t', indent).Append("end\n");
+        }
+	}
+
+	class HardwareModule : HardwareMember
+	{
+        public List<HardwareModuleParam> Parameters { get; } = new List<HardwareModuleParam>();
+        public List<HardwareEvent> Events { get; } = new List<HardwareEvent>();
+		public List<HardwareMember> Members { get; } = new List<HardwareMember>();
+
+		public override void EmitDeclaration(StringBuilder sb, ref int indent)
+		{
+			sb.Append('\t', indent).Append("module ");
+            Emit(sb, ref indent);
+			sb.Append(" (\n");
+			indent++;
+			foreach (var m in Members)
+			{
+                if(m.IsPublic)
+				    m.EmitAsOutput(sb, ref indent);
+			}
+			sb.Append(");\n");
+			foreach (var m in Members)
+			{
+                m.EmitDeclaration(sb, ref indent);
+			}
+            foreach(var e in Events)
+                e.EmitDeclaration(sb, ref indent);
+			indent--;
+            sb.Append('\t', indent).Append("endmodule\n");
+        }
+	}
+}
--- OhdlCompiler/Program.cs
+++ OhdlCompiler/Program.cs
@@ -2,8 +2,27 @@
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+
+
+namespace Parent1
+{
+	namespace Child1
+	{
+		class Type1 { }
+    }
+
+	namespace Child2
+	{
+		class Type2
+		{
+			private Child1.Type1 foo;
+		}
+	}
+
+}
 
 namespace OhdlCompiler
 {
@@ -12,106 +31,150 @@ namespace OhdlCompiler
 
 		static void Main(string[] args)
 		{
-			var tree = CSharpSyntaxTree.ParseText(@"
+			var compiler = new Compiler();
+            compiler.LoadFile(@"
 using Foo.Bar.Baz;
 class Counter<INT N> {
 	public bit[N] count { get; private set; }
-	public bit reset;
+	public MyNs.MyType reset;
 	
 	event (true) => count = reset ? 0 : count + 1;
 }
 ");
-			var errors = tree.GetRoot().GetDiagnostics();
-			/*var compilation = CSharpCompilation.Create("HelloWorld")
-											   .AddReferences(
-													MetadataReference.CreateFromFile(
-														typeof(object).Assembly.Location))
-											   .AddSyntaxTrees(tree);
-			var model = compilation.GetSemanticModel(tree);
-			var root = (CompilationUnitSyntax)tree.GetRoot();
-			var symbol = model.GetTypeInfo(root.Members[0]);*/
 
+            compiler.ResolveTypes();
 
 		}
 	}
-	
-    abstract class CompilerContext
-    {
-        public CompilerContext Parent { get; private set; }
-	    public virtual IEnumerable<CompilerContext> Children => ChildrenMap.Values;
+
+
+    /*
+     * Get using contexts, set for members
+     * Add type definitions
+     * Resolve member types
+     * HARDWARE MODEL
+     * 
+     * 
+     * */
+    
+
+    
+
+	abstract class Binding
+	{
+		private Binding parent;
+		public virtual Binding Parent => parent;
         public virtual string Name { get; }
+		public virtual IEnumerable<Binding> Children => null;
+
+		private static void NameToParts(NameSyntax name, List<string> parts)
+		{
+			if (name == null) return;
+			if (name is QualifiedNameSyntax qn)
+			{
+				NameToParts(qn.Left, parts);
+				NameToParts(qn.Right, parts);
+			}
+			else if (name is SimpleNameSyntax sn)
+			{
+				parts.Add(sn.Identifier.Text);
+			}
+			else throw new Exception("Unknown name node " + name);
+		}
+
+		public virtual void AddChild(Binding child)
+		{
+			if (child == null) return;
+			child.parent = this;
+		}
+
+		public static ImmutableArray<string> GetName(NameSyntax name)
+		{
+			var list = new List<string>();
+			NameToParts(name, list);
+			return ImmutableArray.Create(list.ToArray());
+		}
+
+		public Binding GetRoot()
+		{
+			var c = this;
+			while (c.Parent != null)
+				c = c.Parent;
+			return c;
+		}
+
+        private ImmutableArray<string>? fullyQualifiedName;
+		public virtual ImmutableArray<string> FullyQualifiedName
+		{
+			get
+			{
+				if (fullyQualifiedName.HasValue) return fullyQualifiedName.Value;
+				if (Parent == null) fullyQualifiedName = ImmutableArray.Create(Name);
+				else fullyQualifiedName = Name == null ? Parent.FullyQualifiedName : Parent.FullyQualifiedName.Add(Name);
+				return fullyQualifiedName.Value;
+			}
+		}
+
+        public abstract void ResolveTypes();
+
+		public virtual Binding GetMember(ImmutableArray<string> name, bool isInternal) => null;
+
+		protected Binding(string name)
+		{
+			Name = name;
+		}
+    }
+	
+    abstract class CompilerContext : Binding
+    {
+	    public override IEnumerable<Binding> Children => ChildrenMap.Values;
 
 	    public virtual void AddChild(CompilerContext child)
 	    {
 		    if (child == null) return;
+		    base.AddChild(child);
 		    ChildrenMap[child.Name] = child;
-		    child.Parent = this;
-	    }
-
-	    private static void NameToParts(NameSyntax name, List<string> parts)
-	    {
-		    if (name == null) return;
-		    if (name is QualifiedNameSyntax qn)
-		    {
-			    NameToParts(qn.Left, parts);
-			    NameToParts(qn.Right, parts);
-		    }
-		    else if (name is SimpleNameSyntax sn)
-		    {
-			    parts.Add(sn.Identifier.Text);
-		    }
-		    else throw new Exception("Unknown name node " + name);
-	    }
-
-	    public static ImmutableArray<string> GetName(NameSyntax name)
-	    {
-		    var list = new List<string>();
-            NameToParts(name, list);
-		    return ImmutableArray.Create(list.ToArray());
-	    }
-
-        private ImmutableArray<string>? fullyQualifiedName;
-	    public virtual ImmutableArray<string> FullyQualifiedName
-	    {
-		    get
-		    {
-			    if (fullyQualifiedName.HasValue) return fullyQualifiedName.Value;
-			    if (Parent == null) fullyQualifiedName = ImmutableArray.Create(Name);
-			    else fullyQualifiedName = Name == null ? Parent.FullyQualifiedName : Parent.FullyQualifiedName.Add(Name);
-			    return fullyQualifiedName.Value;
-		    }
 	    }
 
 	    protected readonly Dictionary<string, CompilerContext> ChildrenMap
             = new Dictionary<string, CompilerContext>();
 
-	    public CompilerContext GetMember(ImmutableArray<string> parts)
-	    {
-		    if (parts.Length == 0) return this;
-            if (!MemberCache.TryGetValue(parts, out var ret))
-		    {
-                MemberCache.Add(parts, ret = GetMemberUncached(parts));
-		    }
-		    return ret;
-	    }
-
-	    protected CompilerContext GetMemberDirect(string name)
+	    protected CompilerContext GetChild(string name)
 	    {
 		    ChildrenMap.TryGetValue(name, out var d);
 		    return d;
 	    }
 
-	    protected virtual CompilerContext GetMemberUncached(ImmutableArray<string> name)
+	    protected virtual Binding GetMemberUncached(ImmutableArray<string> name, bool isInternal)
         {
-            return GetMemberDirect(name[0])?.GetMember(ImmutableArray.Create(name, 1, name.Length - 1));
-            
-	    }
-        protected readonly Dictionary<ImmutableArray<string>, CompilerContext> MemberCache
-            = new Dictionary<ImmutableArray<string>, CompilerContext>();
+            return GetChild(name[0])?.GetMember(ImmutableArray.Create(name, 1, name.Length - 1), false);
 
-	    protected CompilerContext(string name)
+        }
+
+	    public override Binding GetMember(ImmutableArray<string> parts, bool isInternal)
 	    {
-		    Name = name;
+		    if (parts.Length == 0) return this;
+		    var mc = isInternal ? MemberCacheInternal : MemberCache;
+		    if (!mc.TryGetValue(parts, out var ret))
+		    {
+			    mc.Add(parts, ret = GetMemberUncached(parts, isInternal));
+		    }
+		    return ret;
+	    }
+
+        protected readonly Dictionary<ImmutableArray<string>, Binding> MemberCache
+            = new Dictionary<ImmutableArray<string>, Binding>();
+        protected readonly Dictionary<ImmutableArray<string>, Binding> MemberCacheInternal
+            = new Dictionary<ImmutableArray<string>, Binding>();
+
+	    protected CompilerContext(string name) : base(name)
+	    {
+	    }
+
+	    public override void ResolveTypes()
+	    {
+		    foreach(var c in Children)
+                c.ResolveTypes();
 	    }
     }
 
@@ -131,7 +194,7 @@ class Counter<INT N> {
 		public void Load(IEnumerable<MemberDeclarationSyntax> members, IEnumerable<UsingDirectiveSyntax> usings)
 		{
 			foreach(var uds in usings)
-                AddUsing(GetName(uds.Name), uds.Alias.Name.Identifier.Text);
+                AddUsing(GetName(uds.Name), uds.Alias?.Name.Identifier.Text);
 
 			foreach (var m in members)
 			{
@@ -153,35 +216,77 @@ class Counter<INT N> {
 		public NamespaceContext GetChildNamespace(ImmutableArray<string> name)
 		{
 			if (name.Length == 0) return this;
-			var child = GetMemberDirect(name[0]);
+			var child = GetChild(name[0]);
             if(child == null)
                 AddChild(child = new NamespaceContext(name[0]));
 			var ns = child as NamespaceContext;
 			if (ns == null)
-			{
-				// ERROR: Type already declared
-				return null;
-			}
+			    throw new Exception($"Type {name[0]} is already a namespace name");
 			return ns.GetChildNamespace(ImmutableArray.Create(name, 1, name.Length - 1));
 		}
 
 		public TypeContext AddChildType(TypeDeclarationSyntax type)
 		{
-			var child = GetMemberDirect(type.Identifier.Text);
+			var child = GetChild(type.Identifier.Text);
 			if (child == null)
 			{
 				var tc = new TypeContext(type);
                 AddChild(tc);
 				return tc;
 			}
-			// ERROR: Type or namespace already declared
-			return null;
+			throw new Exception($"Type or namespace {type.Identifier.Text} already defined");
 		}
 
-        protected override CompilerContext GetMemberUncached(ImmutableArray<string> name)
+        protected override Binding GetMemberUncached(ImmutableArray<string> name, bool isInternal)
+        {
+	        var root = GetRoot();
+            // Try to find a type with aliases
+			if (isInternal)
+			{
+				if (Aliases.TryGetValue(name[0], out var aName))
+					return root.GetMember(aName, false) ?? throw new Exception($"The alias {name[0]}={aName} is invalid!");
+			}
+            // Try to find a type directly with the first part
+            if (GetChild(name[0]) is TypeContext type)
+                return type.GetMember(ImmutableArray.Create(name, 1, name.Length - 1), false);
+            if (isInternal)
+			{
+				// Find a type in usings
+				foreach (var u in Usings)
+				{
+                    if (root.GetMember(u.Add(name[0]), false) is TypeContext candidateType)
+                        return candidateType.GetMember(ImmutableArray.Create(name, 1, name.Length - 1), false);
+                }
+			}
+            // Otherwise, look in child namespaces as normal
+            // Failing that, look in the parent namespace
+	        return base.GetMemberUncached(name, false) ?? Parent?.GetMember(name, isInternal);
+        }
+	}
+
+	static class PredefinedTypes
+	{
+		private static readonly Dictionary<string, ImmutableArray<string>> Map = new Dictionary<string, ImmutableArray<string>>
 		{
-            
-			return base.GetMemberUncached(name);
+			{"bit", ImmutableArray.Create("Sys", "Bit")},
+		};
+
+		public static ImmutableArray<string> Find(PredefinedTypeSyntax syntax)
+		{
+			return Map[syntax.Keyword.Text];
+		}
+
+		public static TypeContext GetTypeContext(this TypeSyntax syntax, Binding binding)
+		{
+			switch (syntax)
+			{
+                case PredefinedTypeSyntax predefined:
+	                return (TypeContext)binding.GetRoot().GetMember(Map[predefined.Keyword.Text], false);
+                case QualifiedNameSyntax name:
+	                return (TypeContext)binding.GetMember(Binding.GetName(name), true);
+                default:
+                    throw new Exception($"Invalid syntax {syntax}");
+			}
 		}
 	}
 
@@ -193,13 +298,148 @@ class Counter<INT N> {
         Enum,
 	}
 
+	abstract class ExpressionBinding : Binding
+	{
+		protected ExpressionBinding(string name) : base(name)
+		{
+		}
+
+        public virtual TypeContext ExpressionType { get; protected set; }
+
+		public override Binding GetMember(ImmutableArray<string> name, bool isInternal)
+		{
+			return Parent.GetMember(name, isInternal);
+		}
+    }
+
+	class ParameterBinding : ExpressionBinding
+	{
+		private readonly ParameterSyntax syntax;
+
+		public ParameterBinding(ParameterSyntax syntax) : base(syntax.Identifier.Text)
+		{
+			this.syntax = syntax;
+		}
+
+		public override void ResolveTypes()
+		{
+			ExpressionType = syntax.Type.GetTypeContext(this);
+		}
+	}
+
+	abstract class MemberBinding : ExpressionBinding
+	{
+		protected MemberBinding(string name) : base(name)
+		{
+		}
+
+		public virtual ImmutableArray<string>? ExplicitInterface => null;
+
+    }
+
+	class FieldBinding : MemberBinding
+	{
+		private readonly FieldDeclarationSyntax syntax;
+
+		public FieldBinding(FieldDeclarationSyntax syntax, int index) : base(syntax.Declaration.Variables[index].Identifier
+			.Text)
+		{
+			this.syntax = syntax;
+		}
+
+		public override void ResolveTypes()
+		{
+			ExpressionType = syntax.Declaration.Type.GetTypeContext(this);
+		}
+	}
+
+	class PropertyBinding : MemberBinding
+	{
+		private readonly PropertyDeclarationSyntax syntax;
+
+		public PropertyBinding(PropertyDeclarationSyntax syntax) : base(syntax.Identifier.Text)
+		{
+			this.syntax = syntax;
+		}
+
+		public override void ResolveTypes()
+		{
+			ExpressionType = syntax.Type.GetTypeContext(this);
+		}
+	}
+
+	class MethodGroupBinding : MemberBinding
+	{
+		public MethodGroupBinding(string name) : base(name) { }
+
+		protected readonly List<MethodBinding> Methods = new List<MethodBinding>();
+
+		public override IEnumerable<Binding> Children => Methods;
+
+		public void AddMethod(BaseMethodDeclarationSyntax syntax)
+		{
+			Methods.Add(new MethodBinding(this, syntax));
+		}
+
+		public override void ResolveTypes()
+		{
+			throw new NotImplementedException();
+		}
+	}
+    
+
+	class MethodBinding : Binding
+	{
+		public override Binding Parent { get; }
+
+		private readonly BaseMethodDeclarationSyntax syntax;
+
+		public override string Name => ((MethodGroupBinding) Parent).Name;
+
+		public MethodBinding(MethodGroupBinding parent, BaseMethodDeclarationSyntax syntax) : base(null)
+		{
+            Parent = parent;
+			this.syntax = syntax;
+
+		}
+
+        public TypeContext ReturnType { get; private set; }
+        public ImmutableArray<ParameterBinding> Parameters { get; private set; }
+
+		public override void ResolveTypes()
+		{
+			if (syntax is MethodDeclarationSyntax method)
+				ReturnType = method.ReturnType.GetTypeContext(this);
+			Parameters = ImmutableArray.Create(syntax.ParameterList.Parameters.Select(p => new ParameterBinding(p)).ToArray());
+		}
+	}
+
+	class EventBinding : Binding
+	{
+		public EventBinding(EventDeclarationSyntax syntax) : base("$event")
+		{
+		    
+		}
+
+		public override void ResolveTypes()
+		{
+			
+		}
+	}
+
     class TypeContext : CompilerContext
     {
+        protected readonly Dictionary<string, MemberBinding> Members = new Dictionary<string, MemberBinding>();
+        protected readonly List<EventBinding> Events = new List<EventBinding>();
+
         public ClassType ClassType { get; }
+        public MethodGroupBinding Constructor { get; } = new MethodGroupBinding(".ctor");
 
 	    public TypeContext(TypeDeclarationSyntax syntax) : base(syntax.Identifier.Text)
 	    {
 		    ClassType = GetClassType(syntax.Keyword);
+            foreach(var m in syntax.Members)
+                AddMember(m);
 	    }
 
 	    private static ClassType GetClassType(SyntaxToken keyword)
@@ -218,6 +458,53 @@ class Counter<INT N> {
 				    throw new Exception($"Invalid type declaration keyword {keyword}");
 		    }
         }
+
+	    protected void AddMember(MemberBinding member)
+	    {
+		    if (Members.ContainsKey(member.Name))
+		    {
+			    throw new Exception($"Member {member.Name} in {Name} already exists!");
+		    }
+		    Members[member.Name] = member;
+	    }
+
+	    protected void AddMember(MemberDeclarationSyntax member)
+	    {
+		    switch (member)
+		    {
+                case TypeDeclarationSyntax type:
+                    AddChild(new TypeContext(type));
+	                break;
+                case FieldDeclarationSyntax field:
+                    for(int i = 0, l = field.Declaration.Variables.Count; i < l; i++)
+                        AddMember(new FieldBinding(field, i));
+	                break;
+                case MethodDeclarationSyntax method:
+	                var mName = method.Identifier.Text;
+                    if (!Members.TryGetValue(mName, out var methodGroup))
+                        AddMember(methodGroup = new MethodGroupBinding(mName));
+                    ((MethodGroupBinding)methodGroup).AddMethod(method);
+	                break;
+                case ConstructorDeclarationSyntax ctor:
+                    Constructor.AddMethod(ctor);
+	                break;
+                case PropertyDeclarationSyntax property:
+                    AddMember(new PropertyBinding(property));
+	                break;
+                case EventDeclarationSyntax sEvent:
+                    Events.Add(new EventBinding(sEvent));
+	                break;
+                default:
+                    throw new Exception($"Invalid syntax {member}");
+		    }
+	    }
+
+	    public override void ResolveTypes()
+	    {
+		    base.ResolveTypes();
+		    foreach (var m in Members)
+			    m.Value.ResolveTypes();
+	    }
     }
 
 	class Compiler
@@ -234,6 +521,12 @@ class Counter<INT N> {
 			var root = (CompilationUnitSyntax)tree.GetRoot();
 			rootContext.Load(root.Members, root.Usings);
 		}
+
+		public void ResolveTypes()
+		{
+			rootContext.ResolveTypes();
+		}
+
 
 		
 	}
